19ecb626a1a99b27a00fa21bc691a917aa60b402
felipetodev <fe.ossandon.u@gmail.com>
2021-09-10 00:34:45 -0300

    Add axios interception token in api requests

--- app/src/api/index.js
+++ app/src/api/index.js
@@ -4,7 +4,20 @@ const { VITE_API: API = '' } = import.meta.env
 
 // productsApi
 export const getProducts = async () => await axios.get(`${API}/products`)
-export const saveProduct = async (newProduct = {}) => await axios.post(`${API}/products`, newProduct)
+
+const axios2 = axios.create({
+  baseURL: `${API}/products`
+})
+
+axios2.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${window.localStorage.getItem('token')}`
+
+  return config
+}, (error) => {
+  return Promise.reject(error)
+})
+
+export const saveProduct = async (newProduct = {}) => await axios2.post(`${API}/products`, newProduct)
 
 // authApi
 export const register = async (user) => await axios.post(`${API}/auth/register`, user)
@@ -14,3 +27,6 @@ export const profile = async (token) => await axios.get(`${API}/auth/profile`, {
     Authorization: `Bearer ${token}`
   }
 })
+
+export const deleteProduct = async (productId) => await axios2.delete(`${API}/products/${productId}`)
+export const updateProduct = async (productId) => await axios2.put(`${API}/products/${productId}`)
